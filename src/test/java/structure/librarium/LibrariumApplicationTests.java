f3ddb3f1a89ee119b8350d6cc55557af65283668
helio <helioemvezdisso@gmail.com>
2025-04-16 16:27:29 -0300

    init commit

---
+++ src/test/java/structure/librarium/LibrariumApplicationTests.java
@@ -0,0 +1,13 @@
+package structure.librarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
