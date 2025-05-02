a41a0261f075ee5739d616e8fb74a7a08a03fdbe
BernardoBoechat <Bernardoboechat6@gmail.com>
2025-05-01 21:04:32 -0300

    Adição de info book

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,29 @@
 const userprofile = document.querySelector(".user-profile");
 const drop = document.querySelector(".dropuser");
 
+const cards = document.querySelector(".book-card");
+
+const infocard = document.querySelector("#book-click");
+
+const maincontent = document.querySelector('.main-content');
+
+const backpage = document.querySelector(".backpage");
+
+cards.addEventListener("click", () => {
+    if (infocard.style.display === "") {
+        infocard.style.display = "flex";
+        maincontent.style.display = "none";
+    }
+})
+
+backpage.addEventListener("click", () => {
+    if (infocard.style.display === "flex") {
+        infocard.style.display = "none"
+        maincontent.style.display = "block";
+    }
+})
+
+
 userprofile.addEventListener("click", () => {
     drop.classList.toggle("userflex");
 });
