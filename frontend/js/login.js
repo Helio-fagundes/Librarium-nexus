3c39417257540d2642cd9417d19d0ef82497f70e
BernardoBoechat <Bernardoboechat6@gmail.com>
2025-05-03 01:44:11 -0300

    adicionado login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,1 +1,23 @@
 const btnback = document.querySelector(".btnregister");
+
+const formlogin = document.querySelector(".form-container");
+
+const formregister = document.querySelector(".form-register");
+
+const backlogin = document.querySelector(".btnlogin");
+
+// Vai para o formulário de registro
+btnback.addEventListener("click", () => {
+    formlogin.classList.remove("flex");
+    formlogin.classList.add("hidden");
+    formregister.classList.remove("hidden");
+    formregister.classList.add("flex");
+});
+
+// Volta para o formulário de login
+backlogin.addEventListener("click", () => {
+    formregister.classList.remove("flex");
+    formregister.classList.add("hidden");
+    formlogin.classList.remove("hidden");
+    formlogin.classList.add("flex");
+});
