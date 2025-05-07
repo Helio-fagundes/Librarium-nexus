87cf1467fe51f8d163a73f4e40cdb81888678dae
helio <helioemvezdisso@gmail.com>
2025-05-07 18:49:15 -0300

    fix: erro de retorno de body corrigido

--- src/main/java/structure/librarium/config/web/WebConfig.java
+++ src/main/java/structure/librarium/config/web/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500/", "http://localhost:5500", "http://localhost:3000")
+                .allowedOrigins("http://127.0.0.1:5500/", "http://localhost:5500", "http://localhost:3000", "https://librarium-nexus.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
