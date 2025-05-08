9ae33c2c65c2b3fa781ab510fdc236cca8f7df1b
BernardoBoechat <Bernardoboechat6@gmail.com>
2025-05-07 21:05:28 -0300

    Testes com Web Socket

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -30,7 +30,7 @@ const back = document.querySelector(".voltar");
 chat.addEventListener("click", () => {
     if (msgcontainer.style.display === "none" || msgcontainer.style.display === "") {
         chat.style.display = "none";
-        msgcontainer.style.display = "block"; 
+        msgcontainer.style.display = "block";
     }
 });
 back.addEventListener("click", (e) => {
@@ -38,3 +38,72 @@ back.addEventListener("click", (e) => {
     msgcontainer.style.display = "none";
     chat.style.display = "block";
 });
+
+
+(function () {
+    const socket = new SockJS('http://localhost:3000/chat');
+
+   
+    socket.onopen = function () {
+        console.log('Conectado ao servidor');
+    };
+
+   
+    socket.onmessage = function (e) {
+        console.log('Mensagem recebida:', e.data);
+
+    
+        const chatContainer = document.querySelector('.mensagem-container');
+        const messageDiv = document.createElement('div');
+        messageDiv.classList.add('mensagem');  
+        messageDiv.innerHTML = `
+          <div class="usuario">
+            <div class="letra">M</div>
+            <div class="conteudo">
+                <div class="nome">Maria Oliveira</div>
+                <div class="texto">${e.data}</div>  <!-- Usando e.data para exibir a mensagem -->
+                <div class="hora">07:15</div>
+            </div>
+          </div>
+        `;
+        chatContainer.appendChild(messageDiv);
+
+        messageInput.value = ''; 
+    };
+
+   
+    socket.onclose = function () {
+        console.log('Conexão fechada');
+    };
+
+
+    const sendButton = document.querySelector('.entrada button');
+    const messageInput = document.querySelector('.entrada input');
+
+ 
+    sendButton.addEventListener('click', function () {
+        const message = messageInput.value.trim();
+        if (message) {
+            socket.send(message); 
+            console.log('Mensagem enviada:', message);
+
+            const chatContainer = document.getElementById('insert-chat');
+            const messageDiv = document.createElement('div');
+            messageDiv.classList.add('chat');
+            messageDiv.innerHTML = `
+            <div class="user">
+                <div class="user-chat">
+                    <img src="/img/pessoa.png" alt="Usuário">
+                </div>
+                <div>
+                    <h4>Você</h4>
+                    <p>${message}</p>
+                </div>
+            </div>
+        `;
+            chatContainer.appendChild(messageDiv);
+        }
+    });
+})();
+
+
